ee237de98bdea8578aa823d2efe3091c33279cbf
TJ Phan <t.jamesphan@gmail.com>
2022-05-04 11:49:46 -0700

    add more queries and resolver functions with inline db

--- src/graphQL/hackernews-node/src/index.js
+++ src/graphQL/hackernews-node/src/index.js
@@ -4,15 +4,35 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 // implementation of schema
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
+
 
 // bundling schema and implementation here, in server! 
 const server = new ApolloServer({
